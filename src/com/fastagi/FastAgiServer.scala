a8042a8104afd7aabe596e10bd17b7196330b56b
Akintayo A. Olusegun <trinisoftinc@trinisoftinc.ukn.com>
2008-10-14 14:45:07 +0100

    Refactored Sessions.    Brought out Functions    Refactored readHeader

--- src/com/fastagi/FastAgiServer.scala
+++ src/com/fastagi/FastAgiServer.scala
@@ -1,18 +1,19 @@
 package com.fastagi
 
 import java.net._
+import scala.actors.Actor
 
-class FastAgiServer(port: Int) extends Thread {
+class FastAgiServer(port: Int) extends Actor {
     val server: ServerSocket = new ServerSocket(port)
     var session: Session = null
 
-    override def run(): Unit = {
+    def act() = {
         while(true) {
             println("Waiting for connection")
             val client: Socket = server.accept()
             println("Connected")
             session = new Session(client)
-            session.start()
+            session.start()          
         }
     }
 
--- src/com/fastagi/Session.scala
+++ src/com/fastagi/Session.scala
@@ -8,11 +8,12 @@ import java.util._
 
 class Session(client: Socket) extends Actor {
     
-    val is: InputStream = client.getInputStream()
-    val os: OutputStream = client.getOutputStream()
+    val is = client.getInputStream()
+    val os = client.getOutputStream()
+    val functions = new Functions(this.is, this.os)
 
-    readHeader()
     def act() {
+        this.functions.readHeader()
         loop {
             react {
                 case Messages(data) =>
@@ -22,57 +23,10 @@ class Session(client: Socket) extends Actor {
                             client.close()
                             println("Socket Closed.....")
                             exit()
-                        case _ =>                            
-                            //send message thru client
-
-                            //read response from socket                                   
-                            //send back response to sender
                     }
-                case _ =>
-                    //Send back unknown command response
+                case _ => 
+                    this.functions.echo("Unknown Message")
             }
         }
-    }
-
-    def readHeader() {
-        new Thread() {
-            override def run(): Unit = {
-                var ch: Int = 0
-                var sb: StringBuffer = new StringBuffer()
-                val charSequence: Vector[Int] = new Vector()
-                var streamHasMore = true
-                while(streamHasMore) {
-                    ch = is.read()
-                    val thisChar: Char = ch.asInstanceOf[char]
-                    sb.append(thisChar)
-                    charSequence.add(ch)
-                    if(isEOHeader(charSequence)) streamHasMore = false
-                    if(ch == -1) streamHasMore = false
-                }
-                println("----------Recieved Headers-------\n" + sb.toString()) 
-                val toSend = "Happy Actors\r\n"
-                os.write(toSend.getBytes())
-                os.flush()
-            }
-        }.start()
-    }
-
-    def isEOHeader(charSequence: Vector[Int]): boolean = {
-       val size =  charSequence.size()
-       if(size < 4) return false
-
-       if(size >= 4) {
-            if( (charSequence.get(size - 4) == 13) && 
-                (charSequence.get(size - 3) == 10) && 
-                (charSequence.get(size - 2) == 13) && 
-                (charSequence.get(size - 1) == 10) ) return true
-            if( (charSequence.get(size - 2) == 10) &&
-                (charSequence.get(size - 1) == 10) ) return true                    
-            else return false                
-       }
-       return false
-    }
-
-    def isEOF(charSequence: Vector[Int]) {
     }
 }
