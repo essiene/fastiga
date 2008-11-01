6b9b758551f10e49938fbff3b41d1dfa9b94247e
Akintayo A. Olusegun <trinisoftinc@trinisoftinc.ukn.com>
2008-11-01 08:22:17 +0100

    Refactored WebService   - Each function call now initialises its own URLMaker and JSONPipe

--- lib/src/com/konfirmagi/webservice/WebService.scala
+++ lib/src/com/konfirmagi/webservice/WebService.scala
@@ -2,26 +2,33 @@ package com.konfirmagi.webservice
 
 class WebService() {
     
-    val urlMaker = new URLMaker()
-    val jsonPipe = new JSONPipe()
 
     def setPreConfirmationStatus(accountID: String, chequeNumber: String, confirmationStatus: String, amount: String): boolean = {
+        val urlMaker = new URLMaker()
+        val jsonPipe = new JSONPipe()
+
         val url = urlMaker.url_for("cheque", "prekonfirm", chequeNumber, 
                                     Map("status"->confirmationStatus, "accountid"->accountID, "amount"->amount))
         var retVal = jsonPipe.connect(url)
 
-        return getStatus("Status", retVal)
+        return getStatus("Status", retVal, jsonPipe)
     }
 
     def setConfirmationStatus(accountID: String, chequeNumber: String, confirmationStatus: String, transactionID: String): boolean = {
+        val urlMaker = new URLMaker()
+        val jsonPipe = new JSONPipe()
+
         val url = urlMaker.url_for("cheque", "konfirm", chequeNumber, 
                                     Map("status"->confirmationStatus, "accountid"->accountID, "transactionid"->transactionID))
         var retVal = jsonPipe.connect(url)
 
-        return getStatus("Status", retVal)
+        return getStatus("Status", retVal, jsonPipe)
     }
 
     def getFileName(recorderID: String): String = {
+        val urlMaker = new URLMaker()
+        val jsonPipe = new JSONPipe()
+
         var url = urlMaker.url_for("recorder", "get_filename", recorderID, null)
 
         val retVal = jsonPipe.connect(url)
@@ -39,6 +46,9 @@ class WebService() {
     }
 
     def saveRecordedFile(recorderID: String, fileName: String, fullPath: String, speechPath: String): boolean = {
+        val urlMaker = new URLMaker()
+        val jsonPipe = new JSONPipe()
+
         val src = fullPath + ".ulaw"
         val dest = speechPath + fileName + ".ulaw"
 
@@ -46,27 +56,33 @@ class WebService() {
 
         val retVal = jsonPipe.connect(url)
         
-        return getStatus("Status", retVal)
+        return getStatus("Status", retVal, jsonPipe)
     }
 
     def isValid(accountNumber: String, pin: String): boolean = {
+        val urlMaker = new URLMaker()
+        val jsonPipe = new JSONPipe()
+
         val url = urlMaker.url_for("account","isvalid",accountNumber, Map[String, String]("pin"->pin))
 
         val retVal = jsonPipe.connect(url)
 
-        return getStatus("Status", retVal)
+        return getStatus("Status", retVal, jsonPipe)
         
     }
 
     def changePin(accountNumber: String, newPin: String): boolean = {
+        val urlMaker = new URLMaker()
+        val jsonPipe = new JSONPipe()
+
         val url = urlMaker.url_for("account", "change_pin", accountNumber, Map[String, String]("newpin"->newPin))
 
         val retVal = jsonPipe.connect(url)
         
-        return getStatus("Status", retVal)
+        return getStatus("Status", retVal, jsonPipe)
     }
 
-    def getStatus(status: String, retVal: String): boolean = {
+    def getStatus(status: String, retVal: String, jsonPipe: JSONPipe): boolean = {
         if(retVal.equals("CONNECTED")) {
             val status = jsonPipe.get("Status")
             if(status.equals("OK")) 
